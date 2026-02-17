a52c136af54a08f976454d3dcc135410826aa74d
Ricardo Huiscaleo llafquen <ricardohuiscaleollafquen@iMac-de-Ricardo.local>
2026-02-17 16:29:40 -0300

    fix: check_token.php usa get_token_db.php

--- caja3/api/gmail/check_token.php
+++ caja3/api/gmail/check_token.php
@@ -2,7 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
-require_once __DIR__ . '/get_token.php';
+require_once __DIR__ . '/get_token_db.php';
 
 $result = getValidGmailToken();
 
